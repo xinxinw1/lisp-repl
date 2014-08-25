83b6152fb1fdaf5363392450fa63b97f89b4f69f
Xin-Xin Wang <xin-xinw@musiclifephilosophy.com>
2014-08-25 17:00:44 -0400

    initial

--- devel3/lisp-test.lisp
+++
@@ -1,7 +0,0 @@
-(mac display a
-  `(string ,@a))
-
-(def display-list-contents (a)
-  (if (null? (cdr a)) (display (car a))
-      (atom? (cdr a)) (+ (display (car a)) " . " (display (cdr a)))
-      (+ (display (car a)) " " (display-list-contents (cdr a)))))
